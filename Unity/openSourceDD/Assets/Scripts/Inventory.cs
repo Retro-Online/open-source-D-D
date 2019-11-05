3d1677278ad7907adf1b686e35a0eb5c6bbc8944
Josiah <tkjarmstrong@gmail.com>
2019-11-04 17:52:24 -0800

    working inventory base
    the inventory system taken from a tutorial that I want to edit and customize without accidentally screwing up

---
+++ Unity/openSourceDD/Assets/Scripts/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+  
+    private bool inventoryEnabled;
+    public GameObject inventory;
+
+    private int allSlots;
+    private int enabledSlots;
+    private GameObject[] slot;
+
+    public GameObject slotHolder;
+    void start()
+    {
+        allSlots = 40;
+        slot = new GameObject[allSlots];
+
+        for (int i = 0; i < allSlots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            inventoryEnabled = !inventoryEnabled;
+
+        if (inventoryEnabled == true)
+        {
+            inventory.SetActive(true);
+        }
+        else{
+            inventory.SetActive(false);
+        }
+    }
+}
